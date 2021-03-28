85190d2c76d6bcb8b9233e2548ba26d28a0c5465
Krzysztof Strauchmann <kris202kris@gmail.com>
2021-03-28 16:32:28 +0200

    Bootstrap and jquery libraries. Configuration Startup and RestClient

---
+++ DietMealApp.Core/Services/RestClient.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietMealApp.Core.Services
+{
+	public class RestClient
+	{
+		/*private readonly IHttpContextAccessor _httpContextAccessor;
+		public RestClient(IHttpContextAccessor httpContextAccessor) =>
+			_httpContextAccessor = httpContextAccessor;*/
+
+		private static readonly HttpClient _client = new HttpClient();
+		private string _apiLink { get; set; }
+		public RestClient(string apiLink) => _apiLink = apiLink;
+
+		public string ResponseMessage { get; set; }
+		public bool IsSuccessful { get; set; }
+		public HttpResponseMessage Response { get; set; }
+
+
+		/// <summary>
+		/// Asynchroniczne wysłanie requestu do ApiLink(appsettings.json)/endpoint 
+		/// </summary>
+		/// <param name="endpoint"></param>
+		/// <param name="method"></param>
+		/// <param name="data">String do wysłania</param>
+		/// <returns>Status</returns>
+		public async Task<bool> RequestAsync(string endpoint, HttpMethod method, string data, string senderId)
+			=> await makeRequest(endpoint, method, data, senderId);
+
+		/// <summary>
+		/// Asynchroniczne wysłanie requestu do ApiLink(appsettings.json)/endpoint 
+		/// </summary>
+		/// <param name="endpoint"></param>
+		/// <param name="method"></param>
+		/// <param name="data">Obiekt do wysłania</param>
+		/// <returns>Status</returns>
+		public async Task<bool> RequestAsync(string endpoint, HttpMethod method, object data, string senderId)
+			=> await makeRequest(endpoint, method, data, senderId);
+
+		/// <summary>
+		/// Asynchroniczne wysłanie requestu do ApiLink(appsettings.json)/endpoint 
+		/// </summary>
+		/// <param name="endpoint"></param>
+		/// <param name="method"></param>
+		/// <param name="data">Obiekt do wysłania (zostanie zserializowany)</param>
+		/// <returns>Status</returns>
+		public async Task<bool> RequestAsync(string endpoint, HttpMethod method, MultipartFormDataContent data, string senderId)
+			=> await makeRequest(endpoint, method, data, senderId);
+
+
+		private async Task<bool> makeRequest(string endpoint, HttpMethod method, object data, string senderId)
+		{
+			if (data is string && data as string == "")
+				data = null;
+			ResponseMessage = null;
+			Response = null;
+			IsSuccessful = false;
+
+
+			try
+			{
+				var uri = new Uri(_apiLink + endpoint);
+				Console.WriteLine(uri.ToString());
+				var request = new HttpRequestMessage(method, uri);
+
+				//var senderId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+				request.Headers.Add("SenderId", senderId);
+				if (data != null)
+					if (data is string)
+						request.Content = new StringContent(data as string, Encoding.UTF8, "application/json");
+					else if (data is MultipartFormDataContent)
+						request.Content = data as MultipartFormDataContent;
+					else
+						request.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+
+				Response = await _client.SendAsync(request);
+				ResponseMessage = await Response.Content.ReadAsStringAsync();
+
+				IsSuccessful = Response.IsSuccessStatusCode;
+
+				return true;
+			}
+			catch (HttpRequestException ex)
+			{
+				Console.WriteLine(ex.InnerException.Message);
+				Response = new HttpResponseMessage(System.Net.HttpStatusCode.ServiceUnavailable);
+				ResponseMessage = ex.Message;
+				return false;
+			}
+		}
+	}
+}
--- DietMealApp/Startup.cs
+++ DietMealApp/Startup.cs
@@ -1,17 +1,29 @@
+using DietMealApp.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace DietMealApp
 {
     public class Startup
     {
+        private readonly List<CultureInfo> _SupportedCultures = new List<CultureInfo>
+        {
+            //new CultureInfo("en-GB"),
+            new CultureInfo("pl-PL")
+        };
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,6 +34,39 @@ namespace DietMealApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region CultureAndLocalizationConfig
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture("pl-PL");
+                options.SupportedCultures = _SupportedCultures;
+                options.SupportedUICultures = _SupportedCultures;
+            });
+            services.Configure<FormOptions>(options =>
+            {
+                options.ValueLengthLimit = int.MaxValue;
+                options.MultipartBodyLengthLimit = long.MaxValue; // if don't set default value is: 128 MB
+                options.MultipartHeadersLengthLimit = int.MaxValue;
+            });
+            //services.AddLocalization();
+            services.AddLocalization(options => options.ResourcesPath = "");
+
+            #endregion
+            #region RestClientConfig
+            services.AddTransient(i =>
+                new RestClient(Configuration["ApiLink"])
+            );
+            #endregion
+            #region MvcConfig
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            //services.AddAuthentication().AddCookie(c =>
+            //{
+            //    c.LoginPath = "/Identity/Account/Login";
+            //});
+            services.AddAuthorization();
+            services.AddMvc().AddDataAnnotationsLocalization().AddViewLocalization();
+            #endregion
             services.AddControllersWithViews();
         }
 
@@ -31,22 +76,42 @@ namespace DietMealApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                //app.UseBrowserLink();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+                app.UseHttpsRedirection();
+                app.UseStatusCodePagesWithRedirects("/Errors/{0}");
+                //app.UseBrowserLink();
             }
+            var localizationOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value;
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pl-PL"),
+                SupportedCultures = _SupportedCultures,
+                SupportedUICultures = _SupportedCultures
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                //.RequireAuthorization();
+                endpoints.MapRazorPages();
             });
         }
     }
