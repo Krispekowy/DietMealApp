035523e75d9526037c83eedd6f1df09989768060
Krzysztof Strauchmann <kris202kris@gmail.com>
2021-06-04 19:20:08 +0200

    Reorganized architecture

---
+++ Service/ApplicationInstallation.cs
@@ -0,0 +1,27 @@
+﻿using DietMealApp.Core.Mappings;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietMealApp.Application
+{
+    public static class ApplicationInstallation
+    {
+        public static IServiceCollection AddDietMealApp(this IServiceCollection services)
+        {
+            #region AutoMapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddSingleton(AutoMapperConfig.RegisterMappings());
+            #endregion
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
