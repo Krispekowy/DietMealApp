c035feb5926d3951020dfb9790f874c993470f60
Krzysztof Strauchmann <kris202kris@gmail.com>
2023-02-08 19:16:15 +0100

    Create role functionality

---
+++ DietMealApp/Controllers/AdministrationController.cs
@@ -0,0 +1,53 @@
+﻿using DietMealApp.Core.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DietMealApp.WebClient.Controllers
+{
+    public class AdministrationController : _ParentController
+    {
+        private readonly RoleManager<IdentityRole<Guid>> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole<Guid> identityRole = new IdentityRole<Guid>
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("index", "home");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
